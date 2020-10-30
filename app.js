41dd93f8503cc5b94cc0c6aab10f82b1d566df6d
eaclumpkens <eaclumpkens@gmail.com>
2020-10-30 12:00:37 -0400

    express server basic setup

---
+++ app.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+// DATA PARSER
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// FILE ROUTES
+app.get("*", function(req, res) {
+    return res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    return res.sendFile(path.join(__dirname, "/notes/notes.html"))
+});
+
+app.get("/api/notes", function(req, res) {
+    return res.sendFile(path.join(res.json(__dirname, "/db/db.json")));
+});
+
+app.get("/api/notes/:note", function(req, res) {
+    
+})
+
+// LISTENING
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+});
