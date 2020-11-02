55cdb0ca7ccfa4055eb5eadc7b6538a50b54dc12
eaclumpkens <eaclumpkens@gmail.com>
2020-11-02 18:11:49 -0500

    troubleshooting js files, basic functionality reached

---
+++ server.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const path = require("path");
+const shortid = require("shortid");
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+// DATA PARSER
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(__dirname + "/public" ));
+
+// GET REQUESTS
+app.get("/", function(req, res) {
+    return res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    return res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    return res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+app.post("/api/notes", function(req, res) {
+	let data = fs.readFileSync("db/db.json");
+	let array = JSON.parse(data);
+	let newNote = {
+		id: shortid.generate(),
+		title: req.body.title,
+		text: req.body.text,
+	};
+
+	array.push(newNote);
+	data = JSON.stringify(array);
+	fs.writeFileSync("db/db.json", data);
+	res.send({ msg: "Note saved to the database!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+	let data = fs.readFileSync("db/db.json");
+    let array = JSON.parse(data);
+    let note = req.params.id;
+    
+    console.log(array);
+
+	if (!note) {
+		return res.status(400).send("The note with the given id was not found.");
+    }
+    
+	const index = array.indexOf(note);
+	array.splice(index, 1);
+	data = JSON.stringify(array);
+	fs.writeFileSync("db/db.json", data);
+	res.send({ msg: "Note deleted from the database!" });
+});
+
+
+// LISTENING
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+});
